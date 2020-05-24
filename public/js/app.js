23abd095e0f4f404510754cbca0372642a4484cb
WON-SANG YOO <won-sangyoo@WON-SANGs-MacBook-Air-4.local>
2020-05-24 16:35:56 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,17 +8,15 @@ weatherForm.addEventListener('submit', (e) => {
 
   const location = search.value;
 
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = '';
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = '';
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
