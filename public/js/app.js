f03da632f861b243fd8fab55d376ec4783c2d1e4
WON-SANG YOO <won-sangyoo@WON-SANGs-MacBook-Air-4.local>
2020-05-24 16:43:04 -0400

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,13 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const location = search.value;
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
 
   fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
-        messageTwo.textContent = '';
       } else {
         messageOne.textContent = data.location;
         messageTwo.textContent = data.forecast;
